c45863ab0d086db1088942e6b1f1de62c19a1dfb
오예진 <ooyejinn@gmail.com>
2024-09-10 15:22:46 +0900

    feat: Button 컴포넌트 개발

--- frontend/bibliophile/src/components/Button.tsx
+++ frontend/bibliophile/src/components/Button.tsx
@@ -1,34 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface ButtonProps {
-  onClick: () => void;
   label: string;
-  styleType?: "primary" | "secondary" | "danger";
+  onClick?: () => void;
   disabled?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({
-  onClick,
-  label,
-  styleType = "primary",
-  disabled = false,
-}) => {
-  const getButtonClass = () => {
-    switch (styleType) {
-      case "primary":
-        return "btn-primary";
-      case "secondary":
-        return "btn-secondary";
-      case "danger":
-        return "btn-danger";
-      default:
-        return "";
-    }
-  };
+const Button: React.FC<ButtonProps> = ({ label, onClick, disabled=false }) => {
+
+  const baseClass = 'w-full h-10 rounded-[5px] bg-orange text-black shadow-custom';
+  const activeClass = 'active:bg-activeOrange active:text-white active:shadow-customInner';
+  const disabledClass = 'disabled:bg-lightGray disabled:text-black';
+  const buttonClass = `${baseClass} ${activeClass} ${disabledClass}`;
+
+  const textClass = 'text-base font-regular';
 
   return (
-    <button className={`button ${getButtonClass()}`} onClick={onClick} disabled={disabled}>
-      {label}
+    <button
+      className={buttonClass}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      <span className={textClass}>
+        {label}
+      </span>
     </button>
   );
 };
