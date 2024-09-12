4c23688ded4e1b57d93e058ccabbf1fd9de22806
yewon <yewone0428@naver.com>
2024-09-12 17:29:50 +0900

    feature: Bottom Sheet 스톱워치 컴포넌트 개발

--- frontend/bibliophile/src/components/BottomSheet/BottomSheetStopwatch.tsx
+++ frontend/bibliophile/src/components/BottomSheet/BottomSheetStopwatch.tsx
@@ -5,6 +5,8 @@ const BottomSheetStopwatch: React.FC = () => {
   const [running, setRunning] = useState<boolean>(true);
   const [time, setTime] = useState<number>(0);
 
+  const total_reading_time: string = "00:20:23";
+
   useEffect(() => {
     let interval: NodeJS.Timeout | null = null;
     if (running) {
@@ -24,18 +26,20 @@ const BottomSheetStopwatch: React.FC = () => {
     return `${getHours}:${getMinutes}:${getSeconds}`;
   };
 
-  const handleButtonClick = () => {
+  const handleClickButton = () => {
     setRunning(!running);
   };
 
   return (
     <BottomSheet height={600}>
       <div className="flex flex-col items-center justify-center m-[20%]">
-        <p className="font-light text-base leading-normal">그동안 읽은 시간 : 00:20:22</p>
+        <p className="font-light text-base leading-normal">
+          그동안 읽은 시간 : {total_reading_time}
+        </p>
         <img className="my-[10%]" src="./src/assets/book-reading-fox.gif" alt="책 읽는 여우" />
         <p className="font-light text-base leading-normal">다시 요리를 시작한지 ...</p>
         <p className="font-bold text-4xl leading-normal mb-[10%]">{formatTime(time)}</p>
-        <div onClick={handleButtonClick}>
+        <div onClick={handleClickButton}>
           {running ? (
             <i className="fi fi-rr-pause-circle text-6xl text-active-orange pt-2"></i>
           ) : (
