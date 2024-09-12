10a4f6c8c743b3e63318e132b070c11212ac179b
오예진 <ooyejinn@gmail.com>
2024-09-12 16:12:54 +0900

    fix: 컴포넌트 내에서 좋아요 함수 처리되도록 수정, 이름 수정

--- frontend/bibliophile/src/components/common/LikeButton.tsx
+++ frontend/bibliophile/src/components/common/LikeButton.tsx
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 
 interface LikeButtonProps {
-  originLiked?: boolean;
-  onLikeChange: (liked: boolean) => void;
+  isBookmarked?: boolean;
+  bookmarkId: string;
 }
 
-const LikeButton: React.FC<LikeButtonProps> = ({ originLiked = false, onLikeChange }) => {
-  const [liked, setLiked] = useState(originLiked);
+const LikeButton: React.FC<LikeButtonProps> = ({ isBookmarked = false, bookmarkId }) => {
+  const [bookmarked, setBookmarked] = useState(isBookmarked);
 
-  const handleToggleLike = () => {
-    const updateLikedState = !liked;
-    setLiked(updateLikedState);
-    onLikeChange(updateLikedState);
+  const handleToggleBookmarked = () => {
+    const updateBookmarkedState = !bookmarked;
+    // TODO: API 연결 시에 bookmarked 의 T/F 여부에 따라 다르게 API 연결하기
+    setBookmarked(updateBookmarkedState);
   };
 
   return (
-    <button onClick={handleToggleLike}>
-      {liked ? (
+    <button onClick={handleToggleBookmarked}>
+      {bookmarked ? (
         <i className="fi fi-sr-heart text-orange text-xl" />
       ) : (
         <i className="fi fi-sr-heart text-soft-gray text-xl" />
