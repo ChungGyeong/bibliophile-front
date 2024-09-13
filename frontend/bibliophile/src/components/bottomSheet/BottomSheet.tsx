e908a6c049ec6456dbd36cddf5775706b3dffc07
yewon <yewone0428@naver.com>
2024-09-13 13:26:04 +0900

    fix: 닫힘 버튼 사이즈 조절

--- frontend/bibliophile/src/components/bottomSheet/BottomSheet.tsx
+++ frontend/bibliophile/src/components/bottomSheet/BottomSheet.tsx
@@ -21,7 +21,7 @@ const BottomSheet: React.FC<BottomSheetProps> = ({ height, children }) => {
       className={`w-[360px] ${getHeightClass()} fixed bg-white border border-gray rounded-lg shadow-md`}
     >
       <i
-        className="fi fi-rr-cross-small p-1 absolute top-[10px] right-[10px] text-[15px]"
+        className="fi fi-rr-cross-small p-1 absolute top-[10px] right-[10px] text-[25px]"
         onClick={handleClose}
       ></i>
       {children}
