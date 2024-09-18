0ece461cf278311e6a0c7c77b89ec72724fd65fe
yewon <yewone0428@naver.com>
2024-09-18 23:07:30 +0900

    feat: 소셜 로그인 버튼 개발

---
+++ frontend/bibliophile/src/components/common/SocialLoginButton.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import kakao from '../../assets/login/kakao.png';
+import google from '../../assets/login/google.png';
+import naver from '../../assets/login/naver.png';
+
+const API_BASE_URL = '../api/oauth/login';
+
+const SocialLoginButton: React.FC = () => {
+  const handleClickSocialLogin = (oauthServerType: string) => {
+    // const loginUrl = `${API_BASE_URL}/${oauthServerType}`;
+    // window.location.href = loginUrl;
+    alert(oauthServerType)
+  };
+
+  return (
+    <div  className="flex justify-center items-center">
+      <div className="flex gap-[30px]">
+        <button onClick={() => handleClickSocialLogin('kakao')} className="flex w-[60px] h-[60px] justify-center items-center flex-shrink-0 rounded-full">
+          <img src={kakao} alt="카카오 로그인" className="w-[60px] h-[60px] rounded-full" />
+        </button>
+        <button onClick={() => handleClickSocialLogin('google')} className="flex w-[60px] h-[60px] justify-center items-center flex-shrink-0 rounded-full">
+          <img src={google} alt="구글 로그인" className="w-[60px] h-[60px] rounded-full" />
+        </button>
+        <button onClick={() => handleClickSocialLogin('naver')} className="flex w-[60px] h-[60px] justify-center items-center flex-shrink-0 rounded-full">
+          <img src={naver} alt="네이버 로그인" className="w-[60px] h-[60px] rounded-full" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default SocialLoginButton;
