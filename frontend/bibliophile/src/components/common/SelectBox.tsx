1a92459ac8e0cd59c5edfb73e15ba08b9843d661
오예진 <ooyejinn@gmail.com>
2024-09-25 14:36:05 +0900

    css: CSS 스타일 수정

--- frontend/bibliophile/src/components/common/SelectBox.tsx
+++ frontend/bibliophile/src/components/common/SelectBox.tsx
@@ -38,9 +38,9 @@ const SelectBox: React.FC<SelectBoxProps> = ({ options, defaultOption, onSelect 
   };
 
   return (
-    <div className="relative border-common w-full" ref={selectBoxRef}>
+    <div className="relative w-full" ref={selectBoxRef}>
       <div
-        className="h-9 px-[10px] flex justify-between items-center"
+        className={`h-9 px-[10px] flex justify-between items-center border border-gray ${isOpen ? "rounded-t-[5px] rounded-b-none" : "rounded-[5px]"} box-border`}
         onClick={handleClickDropDown}
       >
         <span className="text-sm font-light">{selectedOption || "옵션을 선택해주세요."}</span>
@@ -48,11 +48,11 @@ const SelectBox: React.FC<SelectBoxProps> = ({ options, defaultOption, onSelect 
       </div>
 
       {isOpen && (
-        <ul className="absolute w-full bg-white border border-t-0 border-gray rounded-b-[5px] z-1">
-          {options.map(option => (
+        <ul className="absolute w-full bg-white border border-t-0 border-gray rounded-b-[5px] box-border">
+          {options.map((option, index) => (
             <li
               key={option}
-              className={`h-9 px-[10px] flex items-center text-sm font-light border-t border-gray
+              className={`h-9 px-[10px] flex items-center text-sm font-light ${index !== options.length - 1 ? "border-b" : ""} border-gray
                 ${selectedOption === option ? "text-black" : "text-gray"}`}
               onClick={() => handleSelect(option)}
             >
