1210b35a6ed57326f350810076afc589e4401bd1
ramrami-B <hrb.harper@gmail.com>
2024-10-02 11:04:27 +0900

    refactor: 상수 추출

--- frontend/bibliophile/src/utils/calDate.ts
+++ frontend/bibliophile/src/utils/calDate.ts
@@ -11,10 +11,3 @@ export const formatDate = (date: string): string => {
   const datePart = date.split(" ")[0];
   return datePart.replace(/-/g, "/");
 };
-
-export const formatDateToString = (date: Date): string => {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  return `${year}.${month}.${day}`;
-};
