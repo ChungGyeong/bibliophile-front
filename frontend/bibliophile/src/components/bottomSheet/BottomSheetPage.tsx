21c63ccc941ef45f20208c4fcd368f10fbbe555f
오예진 <ooyejinn@gmail.com>
2024-10-08 09:48:42 +0900

    feat: 유효성 검사 추가

--- frontend/bibliophile/src/components/bottomSheet/BottomSheetPage.tsx
+++ frontend/bibliophile/src/components/bottomSheet/BottomSheetPage.tsx
@@ -8,32 +8,46 @@ interface BottomSheetPageProps {
 
 const BottomSheetPage: React.FC<BottomSheetPageProps> = ({ onSave, totalPage }) => {
   const [inputValue, setInputValue] = useState<string>("");
+  const [errorMessage, setErrorMessage] = useState<string>("");
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
+    setErrorMessage("");
   };
 
   const handleButtonClick = () => {
     const pageNumber = parseInt(inputValue);
-    if (!isNaN(pageNumber) && pageNumber <= totalPage) {
-      onSave(pageNumber);
-    } else {
-      alert(`올바른 페이지 번호를 입력해주세요 (1 - ${totalPage}p)`);
+
+    if (isNaN(pageNumber) || pageNumber < 1 || pageNumber > totalPage) {
+      setErrorMessage(`올바른 페이지를 입력해주세요. 1 - ${totalPage}p`);
+      return;
+    } else if (pageNumber < 1) {
+      setErrorMessage("페이지 번호는 1 이상이어야 합니다.");
+      return;
+    } else if (pageNumber > totalPage) {
+      setErrorMessage(`페이지 번호는 ${totalPage} 이하여야 합니다.`);
+      return;
     }
+    onSave(pageNumber);
   };
 
   return (
     <div>
       <div className="flex flex-col items-center justify-center m-[20%]">
-        <p className="font-bold text-xl leading-normal my-[15%]">페이지를 숫자로 입력하세요</p>
+        <p className="font-bold text-xl leading-normal my-[15%]">페이지를 입력하세요</p>
         <input
           type="number"
           placeholder={`전체 ${totalPage}p`}
-          className="w-full border-b-2 focus:border-black outline-none text-gray-500 text-sm py-2 mb-[30%]"
+          className="w-full border-b-2 focus:border-black outline-none text-gray-500 text-sm py-2"
           value={inputValue}
           onChange={handleInputChange}
         />
-        <Button label="저장하기" handleClickButton={handleButtonClick} />
+        <div className="h-4">
+          {errorMessage && <p className="text-orange text-xs mt-1">{errorMessage}</p>}
+        </div>
+        <div className="mt-12 w-full">
+          <Button label="저장하기" handleClickButton={handleButtonClick} />
+        </div>
       </div>
     </div>
   );
