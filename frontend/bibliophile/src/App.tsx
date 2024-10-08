d27667b4df7c05a67d4b76e6c58dd77607988b54
ramrami-B <hrb.harper@gmail.com>
2024-10-08 17:27:29 +0900

    feat: 페이지 이동 시 언제나 상단으로 이동

--- frontend/bibliophile/src/App.tsx
+++ frontend/bibliophile/src/App.tsx
@@ -1,9 +1,21 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes.tsx";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <AppRoutes />
     </Router>
   );
